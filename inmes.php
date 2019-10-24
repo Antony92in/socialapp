e54875740913a28c2714c89a66624dae7e8a6872
Antony Wizard <antoninkersev@gmail.com>
2019-10-24 16:27:03 +0300

    from start

--- inmes.php
+++ inmes.php
@@ -1,18 +1,28 @@
 <?php
 
-require "db.php";
+$name = $_SESSION['logged_user']->login;
 
-$mess = R::getAssoc( 'SELECT touser, textmes FROM messages' );
+$link = mysqli_connect("ec2-54-247-72-30.eu-west-1.compute.amazonaws.com", "zbkdvrivshuesq", "f8a99eafa9a3e1982aa35726fc67d5d21918d410883542131c0b37c2d77bfcd1", "d1iuf40nr11r3q");
+
+$query = ( 'SELECT * FROM messages WHERE touser = 'Baracus'');
+
+$result = mysqli_query($link, $query);
+//print_r($result);
+if ($result = mysqli_query($link, $query)) {
+    /* извлечение ассоциативного массива */
+    while ($row = mysqli_fetch_assoc($result)) {
+        
+     
+     echo "От :" . $row["fromuser"] . "<br>" . $row["textmes"] . "<br>"?>  
+
+    <form action="message.php " method="GET">
+    <button type="submit">Ответить</button>    
 
 
-foreach ($mess as $key => $value) {
-
-	echo $key . "<br>";
-	echo $value;
-
-	# code...
-}
-
+    <?php echo "<hr>";
+     
+    }
+  }
 
 
 
