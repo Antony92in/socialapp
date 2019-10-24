3c8fa51f2091b5c1383ae99763fd6aed416f128b
Antony Wizard <antoninkersev@gmail.com>
2019-10-24 16:00:59 +0300

    daesesvg

--- inmes.php
+++ inmes.php
@@ -4,21 +4,14 @@ require "db.php";
 
 $querying = R::exec( 'SELECT * FROM messages WHERE touser = 'Baracus'' );
 
-if (isset($querying)) {
-    /* извлечение ассоциативного массива */
-    while ($row = $querying) {
-        
-     
-     echo "От :" . $row["fromuser"] . "<br>" . $row["textmes"] . "<br>" ?>  
-
-    <form action="message.php " method="GET">
-    <button type="submit">Ответить</button>    
+foreach ($variable as $key => $value) {
 
 
-    <?php echo "<hr>";
-     
-    }
-  }
+	echo $key . "<br>";
+
+	echo $value . "<hr>";
+	# code...
+}
 
 
 
