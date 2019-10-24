ed4a7d24658416626ec8c9251c2a6b3bfc27cf6f
Antony Wizard <antoninkersev@gmail.com>
2019-10-24 11:10:06 +0300

    db changes for heroku

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 require "libs/rb.php";
-R::setup( 'mysql:host=localhost;dbname=socialapp',
-        'root', 's1jk7310421wl' );
+R::setup( 'pgsql:host=ec2-54-247-72-30.eu-west-1.compute.amazonaws.com;dbname=d1iuf40nr11r3q',
+        'zbkdvrivshuesq', 'f8a99eafa9a3e1982aa35726fc67d5d21918d410883542131c0b37c2d77bfcd1' );
 
 session_start();
