f4a4da1b3d868f5963da0bc752c93c34e6d2a370
Antony Wizard <antoninkersev@gmail.com>
2019-10-24 15:45:45 +0300

    new

--- inmes.php
+++ inmes.php
@@ -1,44 +1,20 @@
 <?php
 
-$link = mysqli_connect("ec2-54-247-72-30.eu-west-1.compute.amazonaws.com", "zbkdvrivshuesq", "f8a99eafa9a3e1982aa35726fc67d5d21918d410883542131c0b37c2d77bfcd1", "d1iuf40nr11r3q");
+require "db.php";
 
-mysqli_set_charset($link, 'utf8');
+$column = R::getCol( 'SELECT textmes FROM messages' );
 
+$countcol = count($column);
 
-$query = ( "SELECT * FROM messages WHERE touser = 'Baracus' ");
-
-$name = $_SESSION['logged_user']->login;
-
-
-$result = mysqli_query($link, $query);
+for ($i=0; $i < $countcol; $i++) { 
 
 
-
-//print_r($result);
-
-
-if (isset($result)) {
-
-    /* извлечение ассоциативного массива */
-    while ($row = mysqli_fetch_assoc($result)) {
-        
-     
-     echo "От :" . $row["fromuser"] . "<br>" . $row["textmes"] . "<br>" ?>  
-
-    <form action="message.php " method="GET">
-    <button type="submit">Ответить</button>    
+	echo $i;
+	# code...
+}
 
 
-    <?php echo "<hr>";
 
-     
-
-    }
-
-  } else { echo "string";}
-
-/* закрытие соединения */
-//mysqli_close($link);
 
 
 ?>
