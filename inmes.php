48bcebe3f2db48d72aecd8069c33e21b6ac6cabb
Antony Wizard <antoninkersev@gmail.com>
2019-10-24 16:28:51 +0300

    aefau

--- inmes.php
+++ inmes.php
@@ -1,10 +1,12 @@
 <?php
 
-$name = $_SESSION['logged_user']->login;
+
 
 $link = mysqli_connect("ec2-54-247-72-30.eu-west-1.compute.amazonaws.com", "zbkdvrivshuesq", "f8a99eafa9a3e1982aa35726fc67d5d21918d410883542131c0b37c2d77bfcd1", "d1iuf40nr11r3q");
 
-$query = ( 'SELECT * FROM messages WHERE touser = 'Baracus'');
+$name = $_SESSION['logged_user']->login;
+
+$query = ( "SELECT * FROM messages WHERE touser = 'Baracus'");
 
 $result = mysqli_query($link, $query);
 //print_r($result);
